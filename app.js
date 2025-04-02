9b60263f7135dc4ebadbc79e84851f58857169cd
JayJaber <62089050+JayJaber@users.noreply.github.com>
2025-04-02 14:50:29 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -8,34 +8,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Route to get the transcript of a YouTube video based on its ID
 app.get('/transcript/:id', async (req, res) => {
   try {
-    const videoId = req.params.id; // Get video ID from URL parameter
-    if (!videoId) {
-      return res
-        .status(400)
-        .json({ error: 'Please provide a valid video ID.' });
-    }
+    const id = req.params.id;
 
-    // Construct YouTube URL using the video ID
-    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-
-    // Fetch transcript
-    const transcript = await YoutubeTranscript.fetchTranscript(videoUrl);
+    const transcript = await YoutubeTranscript.fetchTranscript(id);
     const text = transcript
       .map((item) => item.text)
       .join(' ')
       .replace(/&amp;#39;/g, "'");
 
-    // Send the transcript text as the response
     res.json({ transcript: text });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch the transcript.' });
+    res.status(500).json({ error: 'Failed to fetch transcript' });
   }
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
